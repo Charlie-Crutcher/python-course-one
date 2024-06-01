4090364b5b41e398c9c32cb37c142d89638234f0
Charlie <109866763+Charlie-Crutcher@users.noreply.github.com>
2024-06-01 20:12:23 +0100

    Add files via upload

---
+++ june-01-lists.py
@@ -0,0 +1,18 @@
+# Python List Methods
+"""
+    .count() - A list method to count the number of occurances of an element in a list.
+    .insert() - A list method to insert an element into a specific index of a list.
+    .pop() - A list method to remove an element from a specific index or form the end of a list.
+    range() - A built-in Python function to create a sequence of integers.
+    len() - A built-in Python function to get the length of a list.
+    .sort() / sorted() - A method and built-in function to sort a list.
+
+    -- Slicing
+    variable = list[0:5] - would select the first through 5th elements
+"""
+
+# Counting in a List
+"""
+
+
+"""
