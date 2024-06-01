e24b5e96ce66c4e1d007d0b5ca16f0b81cf70e77
Charlie <109866763+Charlie-Crutcher@users.noreply.github.com>
2024-06-01 16:44:22 +0100

    Add files via upload

---
+++ may-30-control-flow.py
@@ -0,0 +1,17 @@
+# This file will discuss topics learnt on the 30th May.
+
+# Boolean - A simple True or False statement.
+boolean_example = False
+must_be_capitalized = True
+
+# Boolean Relational Operators : Equals and Not Equals
+equals_example = 1 == 1 # Would equal True
+not_equals_example = 1 != 1 # Would equal False
+# > Greater than
+# >= Greater than or equal to
+# < Less than
+# <= Less than or equal to
+
+# If statements
+if 1 == 1:
+    print("True!")
