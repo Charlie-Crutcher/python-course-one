851ec2ced1f64cb563be3f7e4407efe984ea48bc
Charlie Crutcher <109866763+Charlie-Crutcher@users.noreply.github.com>
2025-02-18 15:38:08 +0000

    Add files via upload

---
+++ lesson_1_hello_world.py
@@ -0,0 +1,75 @@
+#Codecademy python 3 learning documentation.
+#Charlie Crutcher 2025.
+
+#Print function
+print("")
+print("The print() function is used to display things in the console.")
+print("It can display variables, text, numbers etc.")
+print("This can be useful for things such as debugging.")
+print("")
+
+#Comments
+print("Comments ignore the syntax and are for human use.")
+print("")
+print("Comments are important because:")
+print("1. They can provide context")
+print("2. They can help others read the code better")
+print("")
+
+#Strings
+print("Blocks of text are refered to as 'strings'.")
+print("When declaring a string, it must be surrounded by either double OR single quotes ("" or '')")
+print("Multi-line strings can be declared by use of three double OR single quotes either side (''' ''') ")
+print("")
+
+#Variables
+print("In programming, variables are created to store data for reuse.")
+print("This could be a user ID, or an employee name etc.")
+print("To create a variable, you assign it using the equals sign.")
+print("")
+print("An example could be: message_string_example = 'this is a string example.' ")
+print("It could then be changed or altered and reused.")
+print("")
+
+#Errors
+print("Two common errors in python are SyntaxError and NameError.")
+print("SyntaxError: something is wrong with the way the program is written.")
+print("NameError: an unrecognized word, such as an undefined variable.")
+print("")
+
+#Numbers
+print("Python has a few numberic data types, which each have different ways of storing numbers")
+print("int(integer). stores a whole number with no decimal point.")
+print("float(floating point number). stores decimals.")
+print("Sometimes floats can behave in unexpected ways due to how the computers store them.")
+print("This is because they cannot be respresented as binary fractions, and are instead approximated, for example")
+print("1/3 becomes 0.3 or 0.33333, but no matter how many digits you can write, it will never equal exactly 1/3.")
+print("")
+
+#Calculations
+print("Python can perform arithemetic operations extremely quickly and efficiently.")
+print("The Error 'ZeroDivisionError' can occur whilst attempting to divide by 0.")
+print("")
+
+#Order of Operations
+print("Python evaluates expressions in order of precedence as follows:")
+print("1. Items in parentheses (...) ")
+print("2. Exponentiation (**) ")
+print("3. Multiplication and division")
+print("4. Addition and subtraction")
+print("5. Comparison (<, >= etc) ")
+print("6. Equality (== & !=) ")
+print("7. not")
+print("8. and")
+print("9. or")
+print("")
+
+#Concatenation
+print("The + operator can also add two strings as well as two numbers.")
+print("The process of combining two strings is called string concatenation.")
+print("If you want to concatenate a string with a number you will need to convert the number using the str() function.")
+print("")
+
+#Plus equals
+print("A shorthand method for updating variables is the += operator.")
+print("This simply adds to the current value of the variable.")
