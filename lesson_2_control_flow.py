348e0093e550e18c4577bec389d796639721612e
Charlie Crutcher <charliemarkcrutcher@gmail.com>
2025-02-24 18:05:04 +0000

    Create lesson_2_control_flow.py

---
+++ lesson_2_control_flow.py
@@ -0,0 +1,26 @@
+#What is control flow?
+#It can be thought of as a set of checklists, starting from the top and ending at the bottom.
+#Python executes scripts from the top down (iterative), so it is our job to tell the computer when it should execute certain blocks of code.
+
+#Boolean expressions
+print("A boolean expression is a statement that can be true or false")
+print("")
+
+#Relational operators
+print("Boolean expressions can be created by using relational and logical operators")
+print("Relational operators compare two values and return true or false.")
+print("Logical operators are used to combine multiple Boolean expressions")
+print("An example of a relational operator is the equals operator (==)")
+print("")
+
+#If statements
+print("Boolean variables and expressions are the building blocks of conditional statements")
+print("For example, if it is raining, bring an unbrella, if not, bring a t shirt, would be an If statement.")
+print("")
+
+#Relational Operators 2
+print("Some additional relational operators could include:")
+print("> (greater than) and >= (greater than or equal to)")
+print("< (less than) and <= (less than or equal to)")
+print("")
+
